ee919f53016d3b91a35f483ce48a2096e2461589
helmahdy2022 <info@edu-happy.com>
2022-02-24 00:47:44 +0200

    Updates

---
+++ precache-manifest.e4b12fd5689b1374ffd7f4594eb61eb3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d18a341bf67f37ccfd35",
+    "url": "/udacity-myreads/static/css/main.b19f356d.chunk.css"
+  },
+  {
+    "revision": "d18a341bf67f37ccfd35",
+    "url": "/udacity-myreads/static/js/main.d18a341b.chunk.js"
+  },
+  {
+    "revision": "0ec33beb229732abb400",
+    "url": "/udacity-myreads/static/js/1.0ec33beb.chunk.js"
+  },
+  {
+    "revision": "94a001f5414cecc913be",
+    "url": "/udacity-myreads/static/js/runtime~main.94a001f5.js"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/udacity-myreads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/udacity-myreads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/udacity-myreads/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "ee99081a20bd8b71371bf371d2c56961",
+    "url": "/udacity-myreads/index.html"
+  }
+];
